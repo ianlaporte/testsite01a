954ad74a32bbb3e754e9d4da3663106bf880e754
ianlaporte <ianlaporte@gmail.com>
2021-10-23 03:59:29 -0400

    Initialize add-code-of-conduct-1

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const webpack = require("webpack");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  mode: "development",
+  watch: false,
+  entry: ["./_process/js/app.js", "./_process/js/script.js"],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "_site/js/"),
+    filename: "script.js"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
+    new VueLoaderPlugin()
+  ]
+};
